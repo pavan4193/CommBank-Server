d58f1962a15ca583fba357402bee283366739ac1
pavan4193 <75196752+pavan4193@users.noreply.github.com>
2024-10-14 19:40:59 -0500

    mc

--- obj/Debug/net6.0/CommBank.AssemblyInfo.cs
+++ obj/Debug/net6.0/CommBank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e538d3c2c527b40227a304025d726964e07cbb5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommBank-Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
